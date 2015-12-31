39ba436babd90dd1bb9bc6c3ff531fa25dcf4240
Scott Holland <scotthollandjr@gmail.com>
2015-12-30 16:26:37 -0800

    Set up next files for next project

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,1 @@
-$(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    var name = prompt("What is your name?");
-    alert("Hello, " + name + ". How is your day going?");
-  });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-});
